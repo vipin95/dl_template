95741978527756b6c908da0c0acb92da895415ce
vipin95 <mavi.mymail@gmail.com>
2020-10-22 23:28:17 +0530

    add some changes like confirm pass in template

--- src/component/signUp/index.js
+++ src/component/signUp/index.js
@@ -5,19 +5,32 @@ import { Link } from "react-router-dom";
 class SignUp extends React.Component{
   constructor(props){
     super(props);
+    this.state = {ConPassStatus: 0};
     this.matchPassword = this.matchPassword.bind(this);
   }
   matchPassword(){
   
     if (document.getElementById("password").value === document.getElementById("cPassword").value ) 
     {
-        alert("true");
+      this.setState({ConPassStatus: 1});
     }
     else{
-      alert("false");
+      this.setState({ConPassStatus: 2});
     }
   }
   render(){
+    const ConPassStatus = this.state.ConPassStatus;
+    let conformPass;
+    if (ConPassStatus === 0) {
+      conformPass = <input type="password" onChange={this.matchPassword} id="cPassword" required className="form-control mb-3" aria-describedby="emailHelp" placeholder='Confirm password'/>;
+    } else {
+      if(ConPassStatus === 2){
+        conformPass = <input type="password" onChange={this.matchPassword} id="cPassword" required className="form-control mb-3 is-invalid" aria-describedby="emailHelp" placeholder='Confirm password'/>;
+      }
+      else{
+        conformPass = <input type="password" onChange={this.matchPassword} id="cPassword" required className="form-control mb-3 is-valid" aria-describedby="emailHelp" placeholder='Confirm password'/>;
+    }
+  }
     return(
       <div className="container-fluid  ">
         <div className="form_position position-relative" >
@@ -26,9 +39,9 @@ class SignUp extends React.Component{
             <hr/>
             <div className="px-4 mx-4 mt-4">
               <div className="form-group ">
-                <input type="text" required name="email" className="form-control mb-3" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <input type="email" required name="email" className="form-control mb-3" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email"/>
                 <input type="password" id="password" required className="form-control mb-3" aria-describedby="emailHelp" placeholder="Enter password"/>
-                <input type="password" onChange={this.matchPassword} id="cPassword" required className="form-control mb-3" aria-describedby="emailHelp" placeholder='Confirm password'/>
+                {conformPass}
               </div>
               <div className="div_button_forgetPassword">
               <div className="text-center"><button className="btn btn-primary px-4"> Registor </button></div>
