f972267602f401f8ff8fcfb9d7a74e4895324067
vipin95 <mavi.mymail@gmail.com>
2020-12-16 11:22:06 +0530

    add password formate check code not complete

--- src/component/login/index.js
+++ src/component/login/index.js
@@ -2,6 +2,66 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './public/css/index.css';
 
+function pass_formate(){
+  let pass ="Ina @123";
+  let array = pass.split("");
+
+  let small_char = false;
+  let upper_char = false;
+  let number = false;
+  let symbol = false;
+  let length = false;
+  let unexpected_char = false;
+
+  let temp = "";
+  ( (array.length > 8) && (array.length < 16) ) ? (length = true) : (length = false);
+  
+  for(let i=0 ; i < array.length ; ++i){
+    temp = array[i];
+
+    switch(temp){
+      case "@":
+        symbol = true;
+        break;
+      case "#":
+        symbol = true;
+        break;
+      case "*":
+        symbol = true;
+        break;
+      case "$":
+        symbol = true;
+        break;
+      default:
+        if( Number.isInteger(parseInt(temp)) ){
+          number = true;
+          break;
+        }
+        else {
+          if(( temp === temp.toUpperCase() )){
+            upper_char = true;
+            continue;
+          }
+          else{
+            if(( temp === temp.toLowerCase() )){
+              small_char = true;
+              continue;
+            }
+            else unexpected_char = true;
+          }
+        }
+    }
+    if(number && upper_char && small_char && symbol && length){
+      alert("password valid");
+      break;
+    }
+    else{
+      if(i == array.length-1 && unexpected_char && ( !number || !upper_char || !small_char || !symbol || !length ) ){
+        alert("Password not valid");
+      }
+    }
+  }
+}
 function Login(props){
   return(
     <div className="container-fluid  ">
@@ -28,6 +88,7 @@ function Login(props){
           </div>
         </form>
       </div>
+      <button className="btn btn-primary px-4" onClick={pass_formate}>Test</button>
     </div>
   )
 }
---
+++ src/component/login/public/js/password_formate.js
@@ -0,0 +1,5 @@
+function pass_formate(pass){
+    let array_pass = pass.split("");
+    console.log(array_pass);
+}
+export default pass_formate;
